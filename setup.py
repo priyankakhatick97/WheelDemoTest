64a348761499c0eb8d2df38b9a1e49a362ea1647
priyankakhatick97 <priyankakhatick97>
2024-10-16 13:22:08 +0000

    hello_world

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_world',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here
+)
