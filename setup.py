e486240970afe6d54131864e6452c30db8f8fcbb
priyankakhatick97 <priyanka.khatick@tigeranalytics.com>
2024-10-16 21:30:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import hello_world
 
 setup(
-  name='Hello World',
+  name='Myhelloworld',
   version=hello_world.__version__,
   author=hello_world.__author__,
   url='https://databricks.com',
@@ -11,7 +11,7 @@ setup(
   description='my test wheel',
   packages=find_packages(include=['hello_world']),
   entry_points={
-    'group_1': 'run=hello_world.hello:main'
+    'group_1': 'run=hello_world.__main__:main'
   },
   install_requires=[
     'setuptools'
