89678fc86f66cd2dc80d3c4f53ddbecdc3419e29
dinesh-tiger <dinesh.kumar2@tigeranalytics.com>
2024-10-22 15:37:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
 
 import hello_world
+import subprocess
 
+def get_git_version():
+    try:
+        # Get the latest Git tag
+        version = subprocess.check_output(["git", "describe", "--tags"], stderr=subprocess.STDOUT).strip().decode('utf-8')
+        return version
+    except subprocess.CalledProcessError:
+        return "0.0.1"  # Fallback version if no tags are found
+      
 setup(
   name='Myhelloworld',
-  version=hello_world.__version__,
+  version=get_git_version(),
   author=hello_world.__author__,
   url='https://databricks.com',
   author_email='john.doe@databricks.com',
